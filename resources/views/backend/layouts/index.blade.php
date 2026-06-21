8c58a5631dbf4a0014134d5a54f7e9c28cb34a07
TegarSa <imaniashiren6@gmail.com>
2026-06-21 19:02:43 +0700

    Configuration

--- resources/views/backend/layouts/index.blade.php
+++ resources/views/backend/layouts/index.blade.php
@@ -15,7 +15,6 @@
     <title>SIKOPI - Dashboard Admin</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
     <link class="stylesheet" href="{{ asset('backend/css/light.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('backend/css/custom.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
